ae282679b1dcba39c150730e70b4d52d9781893c
unknown <bhavyasrivadapalli@gmail.com>
2025-11-24 14:06:30 +0530

    update api url for production

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
       // options optional in newer mongoose versions
     });
     console.log(`MongoDB Connected: ${conn.connection.host}`);
--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://powerfolio-demo.onrender.com",
 });
 
 // Automatically attach token if it exists
